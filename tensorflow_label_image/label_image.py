285d9754ed67734f0ea24e6497093053d199d1a2
이인근 <20133132@kookmin.ac.kr>
2018-10-08 17:44:39 +0900

    캠에서 직접 읽게 하기위해 read_tensor_from_image_file 함수 수정

--- tensorflow_label_image/label_image.py
+++ tensorflow_label_image/label_image.py
@@ -36,26 +36,26 @@ def load_graph(model_file):
   return graph
 
 
-def read_tensor_from_image_file(file_name,
+def read_tensor_from_image_file(file,
                                 input_height=299,
                                 input_width=299,
                                 input_mean=0,
                                 input_std=255):
-  input_name = "file_reader"
-  output_name = "normalized"
-  file_reader = tf.read_file(file_name, input_name)
-  if file_name.endswith(".png"):
-    image_reader = tf.image.decode_png(
-        file_reader, channels=3, name="png_reader")
-  elif file_name.endswith(".gif"):
-    image_reader = tf.squeeze(
-        tf.image.decode_gif(file_reader, name="gif_reader"))
-  elif file_name.endswith(".bmp"):
-    image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
-  else:
-    image_reader = tf.image.decode_jpeg(
-        file_reader, channels=3, name="jpeg_reader")
-  float_caster = tf.cast(image_reader, tf.float32)
+  # input_name = "file_reader"
+  # output_name = "normalized"
+  # file_reader = tf.read_file(file_name, input_name)
+  # if file_name.endswith(".png"):
+  #   image_reader = tf.image.decode_png(
+  #       file_reader, channels=3, name="png_reader")
+  # elif file_name.endswith(".gif"):
+  #   image_reader = tf.squeeze(
+  #       tf.image.decode_gif(file_reader, name="gif_reader"))
+  # elif file_name.endswith(".bmp"):
+  #   image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
+  # else:
+  #   image_reader = tf.image.decode_jpeg(
+  #       file_reader, channels=3, name="jpeg_reader")
+  float_caster = tf.cast(file, tf.float32)
   dims_expander = tf.expand_dims(float_caster, 0)
   resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
   normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
@@ -127,7 +127,7 @@ if __name__ == "__main__":
   
   graph = load_graph(model_file)
   t = read_tensor_from_image_file(
-      file_name,
+      file,
       input_height=input_height,
       input_width=input_width,
       input_mean=input_mean,
