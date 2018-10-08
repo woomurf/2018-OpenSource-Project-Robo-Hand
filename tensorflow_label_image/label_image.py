85218c70c0777cdd054ffb7634995a67e632a477
이인근 <20133132@kookmin.ac.kr>
2018-10-08 17:40:41 +0900

    argparse 부분 삭제

--- tensorflow_label_image/label_image.py
+++ tensorflow_label_image/label_image.py
@@ -85,36 +85,36 @@ if __name__ == "__main__":
   input_layer = "input"
   output_layer = "InceptionV3/Predictions/Reshape_1"
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--image", help="image to be processed")
-  parser.add_argument("--graph", help="graph/model to be executed")
-  parser.add_argument("--labels", help="name of file containing labels")
-  parser.add_argument("--input_height", type=int, help="input height")
-  parser.add_argument("--input_width", type=int, help="input width")
-  parser.add_argument("--input_mean", type=int, help="input mean")
-  parser.add_argument("--input_std", type=int, help="input std")
-  parser.add_argument("--input_layer", help="name of input layer")
-  parser.add_argument("--output_layer", help="name of output layer")
-  args = parser.parse_args()
-
-  if args.graph:
-    model_file = args.graph
-  if args.image:
-    file_name = args.image
-  if args.labels:
-    label_file = args.labels
-  if args.input_height:
-    input_height = args.input_height
-  if args.input_width:
-    input_width = args.input_width
-  if args.input_mean:
-    input_mean = args.input_mean
-  if args.input_std:
-    input_std = args.input_std
-  if args.input_layer:
-    input_layer = args.input_layer
-  if args.output_layer:
-    output_layer = args.output_layer
+  # parser = argparse.ArgumentParser()
+  # parser.add_argument("--image", help="image to be processed")
+  # parser.add_argument("--graph", help="graph/model to be executed")
+  # parser.add_argument("--labels", help="name of file containing labels")
+  # parser.add_argument("--input_height", type=int, help="input height")
+  # parser.add_argument("--input_width", type=int, help="input width")
+  # parser.add_argument("--input_mean", type=int, help="input mean")
+  # parser.add_argument("--input_std", type=int, help="input std")
+  # parser.add_argument("--input_layer", help="name of input layer")
+  # parser.add_argument("--output_layer", help="name of output layer")
+  # args = parser.parse_args()
+  #
+  # if args.graph:
+  #   model_file = args.graph
+  # if args.image:
+  #   file_name = args.image
+  # if args.labels:
+  #   label_file = args.labels
+  # if args.input_height:
+  #   input_height = args.input_height
+  # if args.input_width:
+  #   input_width = args.input_width
+  # if args.input_mean:
+  #   input_mean = args.input_mean
+  # if args.input_std:
+  #   input_std = args.input_std
+  # if args.input_layer:
+  #   input_layer = args.input_layer
+  # if args.output_layer:
+  #   output_layer = args.output_layer
 
   graph = load_graph(model_file)
   t = read_tensor_from_image_file(
