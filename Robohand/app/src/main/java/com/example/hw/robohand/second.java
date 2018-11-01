74dab6bb8b04fcd3aa805e956269fe3a2096b003
woomurf <dngusdnd@gmail.com>
2018-11-01 14:43:33 +0900

    Serialize test

--- Robohand/app/src/main/java/com/example/hw/robohand/second.java
+++ Robohand/app/src/main/java/com/example/hw/robohand/second.java
@@ -3,10 +3,16 @@ package com.example.hw.robohand;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class second extends AppCompatActivity {
 
     BluetoothSPP2 bt;
+    int state;
+
+    private Button check;
 
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
@@ -14,5 +20,17 @@ public class second extends AppCompatActivity {
 
         Intent in = getIntent();
         bt = (BluetoothSPP2)in.getSerializableExtra("bt");
+        state = bt.getServiceState();
+
+        check = (Button)findViewById(R.id.check);
+
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(second.this,state,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
